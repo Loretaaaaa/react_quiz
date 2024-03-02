61dc3615dbb0927d9ff63b90130dabe36b69ee80
Loreta <badalyan.loreta@gmail.com>
2024-03-02 16:33:52 +0400

    skipAnswer works correctly, some improvements

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -3,12 +3,18 @@ import QuestionTimer from "./QuestionTimer";
 import Answers from "./Answers";
 import QUESTIONS from "../questions";
 
-export default function Question({
-  index,
-  onSelectAnswer,
-  onSkipAnswer,
-}) {
+export default function Question({ index, onSelectAnswer, onSkipAnswer }) {
   const [answer, setAnswer] = useState({ selectedAnswer: "", isCorrect: null });
+
+  let timer = 10000;
+
+  if (answer.selectedAnswer) {
+    timer = 1000;
+  }
+
+  if (answer.isCorrect !== null) {
+    timer = 2000;
+  }
 
   function handleSelectAnswer(answer) {
     setAnswer({
@@ -24,7 +30,7 @@ export default function Question({
 
       setTimeout(() => {
         onSelectAnswer(answer);
-      }, 2000)
+      }, 2000);
     }, 1000);
   }
 
@@ -38,7 +44,12 @@ export default function Question({
 
   return (
     <div id="question">
-      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <QuestionTimer
+        key={timer}
+        timeout={timer}
+        onTimeout={answer.selectedAnswer === "" ? onSkipAnswer : null}
+        mode={answerState}
+      />
       <h2>{QUESTIONS[index].text}</h2>
       <Answers
         answers={QUESTIONS[index].answers}
